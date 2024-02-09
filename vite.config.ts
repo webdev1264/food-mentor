845d97e4dec665f0587088db41461a9c3bbe5af2
webdev <webdev1264@gmail.com>
2024-02-09 13:09:08 +0100

    Vite img optimization plugin implemented

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,48 @@
 import { defineConfig } from "vite";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    ViteImageOptimizer({
+      test: /\.(jpe?g|png|svg)$/i,
+      includePublic: true,
+      logStats: true,
+      ansiColors: true,
+      svg: {
+        multipass: true,
+        plugins: [
+          {
+            name: "preset-default",
+            params: {
+              overrides: {
+                cleanupNumericValues: false,
+                removeViewBox: false,
+              },
+            },
+          },
+          "sortAttrs",
+          {
+            name: "addAttributesToSVGElement",
+            params: {
+              attributes: [{ xmlns: "http://www.w3.org/2000/svg" }],
+            },
+          },
+        ],
+      },
+      png: {
+        quality: 80,
+      },
+      jpeg: {
+        quality: 80,
+      },
+      jpg: {
+        quality: 80,
+      },
+      cache: false,
+      cacheLocation: undefined,
+    }),
+  ],
 });
